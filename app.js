1fffbdb9c9bf3c43dd00d2415eaaa3ecffbdab9d
jereme <jeremejazz15@gmail.com>
2021-05-24 22:42:02 +0800

    add generate tiles

--- app.js
+++ app.js
@@ -1,32 +1,202 @@
+"use strict";
+
+function Creature(species, weight, height, diet){
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.tileWrapper = ['<div class="grid-item">', '</div>'];
+}
+
 
     // Create Dino Constructor
+function Dinosaur(species, weight, height, diet, where, when, fact){
+    Creature.call(this, species, weight, height, diet);
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+    this.info = fact;
 
+}
 
+Dinosaur.prototype = Object.create(Creature.prototype);
+
+Dinosaur.prototype.compare_weight = function(human){
+    let diff = Math.abs(this.weight - human.weight);
+    if (this.weight > human.weight){
+        this.info = this.species +  "weighs " + diff + " lbs more than " + human.name; 
+
+    }else if (this.weight < human.weight){
+        this.info = human.name + " weighs " + diff + " lbs more than " + this.name; 
+    }else if (this.weight === human.weight) {
+        this.info = "Both weighs the same";
+    }
+
+};
+
+Dinosaur.prototype.compare_height = function(human){
+    let diff = Math.abs(this.height - human.height);
+    if (this.height > human.height){
+        this.info = this.species +  " is " + diff + " inches taller than " + human.name; 
+
+    }else if (this.height < human.height){
+        this.info = human.name + " is " + diff + " inches taller than " + this.name; 
+    }else if (this.height === human.height) {
+        this.info = "Both heights the same";
+    }
+};
+
+Dinosaur.prototype.compare_diet = function(human){
+
+    if(this.diet === human.diet){
+        this.info = "Both diet are the same."
+    }else{
+        this.info = this.diet;
+    }
+};
+
+Dinosaur.prototype.generate_tile = function(){
+    let tileStr = this.tileWrapper[0];
+    tileStr += '<h3>' + this.species + '</h3>';
+    tileStr += '<img src="images/' + this.species.toLowerCase() +'.png" alt=""></img>';
+    tileStr += ' <p>' + this.info + '</p>';
+
+    tileStr += this.tileWrapper[1];
+
+    return tileStr;
+    
+};
     // Create Dino Objects
 
 
     // Create Human Object
+function Human(name, weight, height, diet){
+    Creature.call(this, "human", weight, height, diet);
+    this.name = name;
+}
 
-    // Use IIFE to get human data from form
+Human.prototype = Object.create(Creature.prototype);
 
+Human.prototype.generate_tile = function(){
+    
+    let tileStr = this.tileWrapper[0];
+    tileStr += '';
+    tileStr += this.tileWrapper[1];
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    return tileStr;
+};
+
 
     
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+
 
     
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// On button click, prepare and display infographic
+const App = {
+    loadDinos: function () {
+        let self = this;
+        self.animals = [];
+        fetch('dino.json')
+            .catch(function(err){
+                console.error("Error: ", err);
+            })        
+            .then(function(response){
+                return response.json();
+            })
+            .then(function (data){
+                
+                let dinos = data.Dinos;
+                dinos.forEach(function (item) {
+                let dino = new Dinosaur(item.species, item.weight, item.height, item.diet, item.where, item.when, item.fact);
+                self.animals.push(dino);
 
+                });
+                self.generateInfo();
+                self.generateTiles();
+            });
+
+    },
+    generateInfo: function(){ //generate dino info
+        let randomIdx = [];
+
+        while(true){
+            let idx = Math.floor(Math.random() * 7); 
+            if(randomIdx.includes(idx)){
+                continue;
+            }else{
+                randomIdx.push(idx);
+            }
+
+            if(randomIdx.length === 3){
+                break;
+            }
+        }
+
+        
+        this.animals[randomIdx[0]].compare_height(this.human);
+        this.animals[randomIdx[1]].compare_weight(this.human);
+        this.animals[randomIdx[2]].compare_diet(this.human);
+        
+
+    },
+    generateTiles: function () {
 
     // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
 
+    let tiles = "";
+    for (let x = 0; x < this.animals.length; x++){ //9x9 tiles
+        tiles += this.animals[x].generate_tile();
+        //insert human card on 5
+        if (x === 5){
+            tiles += this.human.generate_tile();
+        }
+    }
+    // Add tiles to DOM
+    let grid = document.getElementById("grid");
+    grid.innerHTML = tiles;
+    grid.classList.remove("show");
+    grid.classList.remove("hide");
     // Remove form from screen
 
+        let dinocompare = document.getElementById('dino-compare');
+        dinocompare.classList.add("hide");
+        setTimeout(function(){
+            dinocompare.style.display = "none";
+        },400);
+    },
+    btnClick: function(){
+        
+        let name = document.getElementById('name').value;
+        let feet = document.getElementById('feet').value;
+        let inches = document.getElementById('inches').value;
+        let weight = document.getElementById('weight').value;
+        let diet = document.getElementById('diet').value;
+        //basic validation
+        if(name === "" || feet === "" || inches === "" || weight === "" || diet === ""){
+            alert("Incomplete fields")
+        }
+        let height = (feet * 12) + inches;
+        this.human = new Human(name, weight, height, diet);
 
-// On button click, prepare and display infographic
+        this.loadDinos();
+
+    },
+    main: function () {
+        this.animals = [];
+        
+        // this.loadDinos(); //later
+
+    }
+}
+
+
+
+App.main();
+
+
+// Use IIFE to get human data from form
+let btn = document.getElementById('btn');
+btn.addEventListener("click", function (){
+    App.btnClick();
+
+});
